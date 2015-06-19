7194cdac864b236e488a98b4878341f632561468
baob <coder@onesandthrees.com>
2015-06-19 23:08:47 +0100

    passing redis integer store

--- lib/redis_store/integer_store.rb
+++ lib/redis_store/integer_store.rb
@@ -1,5 +1,26 @@
+require 'redis'
+
 module RedisStore
   class IntegerStore
+
+    attr_reader :store_name
+
+    def initialize(number, opts = {})
+      @store_name = opts[:id] || 'integer_store'
+      redis.set(store_name, number.to_s)
+    end
+
+    def redis
+      @redis ||= Redis.new
+    end
+
+    def set(number)
+      redis.set(store_name, number.to_s)
+    end
+
+    def to_i
+      redis.get(store_name).to_i
+    end
 
   end
 end
--- spec/support/shared_examples/integer_store.rb
+++ spec/support/shared_examples/integer_store.rb
@@ -2,14 +2,14 @@ shared_examples 'an integer store' do
   subject { described_class.new(value) }
 
   context 'when initialized with a value' do
-    let(:value) { double('the value') }
+    let(:value) { 578 }
 
     describe '#to_i' do
       specify { expect(subject.to_i).to eql(value) }
     end
 
     context 'and then set to another value' do
-      let(:another_value) { double('another value') }
+      let(:another_value) { 29 }
 
       describe '#to_i' do
         specify { subject.set(another_value) ; expect(subject.to_i).to eql(another_value) }
