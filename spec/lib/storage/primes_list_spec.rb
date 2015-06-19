cb7e9887541493f91eb5fd05ae7e67d926cf8485
baob <coder@onesandthrees.com>
2015-06-19 18:59:57 +0100

    specs for primes list

---
+++ spec/lib/storage/primes_list_spec.rb
@@ -0,0 +1,9 @@
+require_relative '../../../lib/storage/primes_list'
+
+module Storage
+  describe PrimesList do
+
+    it_behaves_like 'common primes list behaviour'
+
+  end
+end
---
+++ spec/support/shared_examples/primes_list.rb
@@ -0,0 +1,22 @@
+shared_examples 'common primes list behaviour' do
+
+  specify { expect(subject).to respond_to(:add_non_prime) }
+
+  context 'when 3 elements are added in sequence' do
+    let(:initial_elements) { [45, 76, 191] }
+    before(:each) do
+      initial_elements.each { |e| subject << e }
+    end
+
+    describe '#to_a' do
+      specify { expect(subject.to_a).to eql(initial_elements) }
+    end
+
+    describe '#add_non_prime' do
+      specify 'can be called with one argument' do
+        subject.add_non_prime(double('something'))
+      end
+    end
+  end
+
+end
