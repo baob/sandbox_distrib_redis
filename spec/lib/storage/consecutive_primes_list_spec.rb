ff2a90f02d51e9fba5ea0733456f9e007980a306
baob <coder@onesandthrees.com>
2015-06-19 19:19:41 +0100

    apply common primes_list specs to consecutive primes

--- spec/lib/storage/consecutive_primes_list_spec.rb
+++ spec/lib/storage/consecutive_primes_list_spec.rb
@@ -2,38 +2,30 @@ require 'storage/consecutive_primes_list'
 
 module Storage
   describe ConsecutivePrimesList do
-    subject do
-      pl = described_class.new
-      initial_list.each { |p| pl << p } unless initial_list.empty?
-      pl
-    end
 
-    context 'initialised with 3, 2' do
-      let(:initial_list) { [3, 2] }
+    it_behaves_like 'common primes list behaviour'
 
-      describe '#to_a' do
-        specify { expect(subject.to_a).to eql(initial_list.sort) }
+    context 'when initialised' do
+      subject do
+        pl = described_class.new
+        initial_list.each { |p| pl << p } unless initial_list.empty?
+        pl
       end
 
-      describe '#all_results' do
-        specify { expect(subject.all_results).to eql(initial_list.sort) }
-      end
-    end
+      context 'initialised with 3, 2' do
+        let(:initial_list) { [3, 2] }
 
-    context 'initialised with 2, 3' do
-      let(:initial_list) { [2, 3] }
+        describe '#to_a' do
+          specify { expect(subject.to_a).to eql(initial_list.sort) }
+        end
 
-      describe '#to_a' do
-        specify { expect(subject.to_a).to eql(initial_list) }
-      end
-
-      describe '#all_results' do
-        specify { expect(subject.all_results).to eql(initial_list) }
+        describe '#all_results' do
+          specify { expect(subject.all_results).to eql(initial_list.sort) }
+        end
       end
 
-      context 'and 4 is added as a non-prime' do
-        let(:added_non_primes) { [4] }
-        before { added_non_primes.each { |np| subject.add_non_prime(np) } }
+      context 'initialised with 2, 3' do
+        let(:initial_list) { [2, 3] }
 
         describe '#to_a' do
           specify { expect(subject.to_a).to eql(initial_list) }
@@ -43,82 +35,20 @@ module Storage
           specify { expect(subject.all_results).to eql(initial_list) }
         end
 
-        describe '#largest_consecutive_test' do
-          specify { expect(subject.largest_consecutive_test).to eql(added_non_primes.last) }
-        end
-
-        describe '#non_consecutive_tests' do
-          specify { expect(subject.non_consecutive_tests).to be_empty }
-        end
-
-        describe '#non_consecutive_primes' do
-          specify { expect(subject.non_consecutive_primes).to be_empty }
-        end
-
-        context 'and then 5 is added as a prime' do
-          let(:added_primes) { [5] }
-          before { added_primes.each { |p| subject << p } }
-
-          describe '#to_a' do
-            specify { expect(subject.to_a).to eql(initial_list + added_primes) }
-          end
-
-          describe '#all_results' do
-            specify { expect(subject.all_results).to eql(initial_list + added_primes) }
-          end
-
-          describe '#largest_consecutive_test' do
-            specify { expect(subject.largest_consecutive_test).to eql(added_primes.last) }
-          end
-
-          describe '#non_consecutive_tests' do
-            specify { expect(subject.non_consecutive_tests).to be_empty }
-          end
-
-          describe '#non_consecutive_primes' do
-            specify { expect(subject.non_consecutive_primes).to be_empty }
-          end
-        end
-      end
-
-      context 'and 7, 5 are added as a prime out-of-sequence' do
-        let(:added_primes) { [7, 5] }
-        before { added_primes.each { |p| subject << p } }
-
-        describe '#to_a' do
-          specify { expect(subject.to_a).to eql(initial_list) }
-        end
-
-        describe '#all_results' do
-          specify { expect(subject.all_results).to eql(initial_list + added_primes.sort) }
-        end
-
-        describe '#largest_consecutive_test' do
-          specify { expect(subject.largest_consecutive_test).to eql(initial_list.last) }
-        end
-
-        describe '#non_consecutive_tests' do
-          specify { expect(subject.non_consecutive_tests).to match_array(added_primes) }
-        end
-
-        describe '#non_consecutive_primes' do
-          specify { expect(subject.non_consecutive_primes).to match_array(added_primes) }
-        end
-
-        context 'and then 6 and 4 are added as a non-prime out-of-sequence' do
-          let(:added_non_primes) { [6, 4] }
+        context 'and 4 is added as a non-prime' do
+          let(:added_non_primes) { [4] }
           before { added_non_primes.each { |np| subject.add_non_prime(np) } }
 
           describe '#to_a' do
-            specify { expect(subject.to_a).to eql(initial_list + added_primes.sort) }
+            specify { expect(subject.to_a).to eql(initial_list) }
           end
 
           describe '#all_results' do
-            specify { expect(subject.all_results).to eql(initial_list + added_primes.sort) }
+            specify { expect(subject.all_results).to eql(initial_list) }
           end
 
           describe '#largest_consecutive_test' do
-            specify { expect(subject.largest_consecutive_test).to eql(added_primes.max) }
+            specify { expect(subject.largest_consecutive_test).to eql(added_non_primes.last) }
           end
 
           describe '#non_consecutive_tests' do
@@ -128,40 +58,39 @@ module Storage
           describe '#non_consecutive_primes' do
             specify { expect(subject.non_consecutive_primes).to be_empty }
           end
+
+          context 'and then 5 is added as a prime' do
+            let(:added_primes) { [5] }
+            before { added_primes.each { |p| subject << p } }
+
+            describe '#to_a' do
+              specify { expect(subject.to_a).to eql(initial_list + added_primes) }
+            end
+
+            describe '#all_results' do
+              specify { expect(subject.all_results).to eql(initial_list + added_primes) }
+            end
+
+            describe '#largest_consecutive_test' do
+              specify { expect(subject.largest_consecutive_test).to eql(added_primes.last) }
+            end
+
+            describe '#non_consecutive_tests' do
+              specify { expect(subject.non_consecutive_tests).to be_empty }
+            end
+
+            describe '#non_consecutive_primes' do
+              specify { expect(subject.non_consecutive_primes).to be_empty }
+            end
+          end
         end
-      end
 
-
-      context 'and 6 and 4 are added as a non-prime out-of-sequence' do
-        let(:added_non_primes) { [6, 4] }
-        before { added_non_primes.each { |np| subject.add_non_prime(np) } }
-
-        describe '#to_a' do
-          specify { expect(subject.to_a).to eql(initial_list) }
-        end
-
-        describe '#all_results' do
-          specify { expect(subject.all_results).to eql(initial_list) }
-        end
-
-        describe '#largest_consecutive_test' do
-          specify { expect(subject.largest_consecutive_test).to eql(4) }
-        end
-
-        describe '#non_consecutive_tests' do
-          specify { expect(subject.non_consecutive_tests).to match_array([6]) }
-        end
-
-        describe '#non_consecutive_primes' do
-          specify { expect(subject.non_consecutive_primes).to be_empty }
-        end
-
-        context 'and then 7, 5 are added as a prime out-of-sequence' do
+        context 'and 7, 5 are added as a prime out-of-sequence' do
           let(:added_primes) { [7, 5] }
           before { added_primes.each { |p| subject << p } }
 
           describe '#to_a' do
-            specify { expect(subject.to_a).to eql(initial_list + added_primes.sort) }
+            specify { expect(subject.to_a).to eql(initial_list) }
           end
 
           describe '#all_results' do
@@ -169,19 +98,95 @@ module Storage
           end
 
           describe '#largest_consecutive_test' do
-            specify { expect(subject.largest_consecutive_test).to eql(added_primes.max) }
+            specify { expect(subject.largest_consecutive_test).to eql(initial_list.last) }
           end
 
           describe '#non_consecutive_tests' do
-            specify { expect(subject.non_consecutive_tests).to be_empty }
+            specify { expect(subject.non_consecutive_tests).to match_array(added_primes) }
+          end
+
+          describe '#non_consecutive_primes' do
+            specify { expect(subject.non_consecutive_primes).to match_array(added_primes) }
+          end
+
+          context 'and then 6 and 4 are added as a non-prime out-of-sequence' do
+            let(:added_non_primes) { [6, 4] }
+            before { added_non_primes.each { |np| subject.add_non_prime(np) } }
+
+            describe '#to_a' do
+              specify { expect(subject.to_a).to eql(initial_list + added_primes.sort) }
+            end
+
+            describe '#all_results' do
+              specify { expect(subject.all_results).to eql(initial_list + added_primes.sort) }
+            end
+
+            describe '#largest_consecutive_test' do
+              specify { expect(subject.largest_consecutive_test).to eql(added_primes.max) }
+            end
+
+            describe '#non_consecutive_tests' do
+              specify { expect(subject.non_consecutive_tests).to be_empty }
+            end
+
+            describe '#non_consecutive_primes' do
+              specify { expect(subject.non_consecutive_primes).to be_empty }
+            end
+          end
+        end
+
+
+        context 'and 6 and 4 are added as a non-prime out-of-sequence' do
+          let(:added_non_primes) { [6, 4] }
+          before { added_non_primes.each { |np| subject.add_non_prime(np) } }
+
+          describe '#to_a' do
+            specify { expect(subject.to_a).to eql(initial_list) }
+          end
+
+          describe '#all_results' do
+            specify { expect(subject.all_results).to eql(initial_list) }
+          end
+
+          describe '#largest_consecutive_test' do
+            specify { expect(subject.largest_consecutive_test).to eql(4) }
+          end
+
+          describe '#non_consecutive_tests' do
+            specify { expect(subject.non_consecutive_tests).to match_array([6]) }
           end
 
           describe '#non_consecutive_primes' do
             specify { expect(subject.non_consecutive_primes).to be_empty }
           end
-        end
-      end
 
+          context 'and then 7, 5 are added as a prime out-of-sequence' do
+            let(:added_primes) { [7, 5] }
+            before { added_primes.each { |p| subject << p } }
+
+            describe '#to_a' do
+              specify { expect(subject.to_a).to eql(initial_list + added_primes.sort) }
+            end
+
+            describe '#all_results' do
+              specify { expect(subject.all_results).to eql(initial_list + added_primes.sort) }
+            end
+
+            describe '#largest_consecutive_test' do
+              specify { expect(subject.largest_consecutive_test).to eql(added_primes.max) }
+            end
+
+            describe '#non_consecutive_tests' do
+              specify { expect(subject.non_consecutive_tests).to be_empty }
+            end
+
+            describe '#non_consecutive_primes' do
+              specify { expect(subject.non_consecutive_primes).to be_empty }
+            end
+          end
+        end
+
+      end
     end
   end
 end
--- spec/support/shared_examples/primes_list.rb
+++ spec/support/shared_examples/primes_list.rb
@@ -3,7 +3,7 @@ shared_examples 'common primes list behaviour' do
   specify { expect(subject).to respond_to(:add_non_prime) }
 
   context 'when 3 elements are added in sequence' do
-    let(:initial_elements) { [45, 76, 191] }
+    let(:initial_elements) { [2, 3, 4] }
     before(:each) do
       initial_elements.each { |e| subject << e }
     end
@@ -14,7 +14,7 @@ shared_examples 'common primes list behaviour' do
 
     describe '#add_non_prime' do
       specify 'can be called with one argument' do
-        subject.add_non_prime(double('something'))
+        subject.add_non_prime(double('something').as_null_object)
       end
     end
   end
