7f744753ecd45ecb09d04955d3fd8f68f6fb1850
baob <coder@onesandthrees.com>
2015-06-19 22:06:00 +0100

    spec for integer queue

--- spec/support/shared_examples/integer_queue.rb
+++ spec/support/shared_examples/integer_queue.rb
@@ -1,13 +1,58 @@
 shared_examples 'an integer queue' do
 
+  context 'initialized with no arguments' do
+    describe '#empty?' do
+      specify { expect(subject.empty?).to be_truthy }
+    end
+  end
+
   context 'when 3 elements are added in sequence' do
     let(:initial_elements) { [2, 3, 4] }
     before(:each) do
       initial_elements.each { |e| subject << e }
     end
 
+    describe '#empty?' do
+      specify { expect(subject.empty?).to be_falsey }
+    end
+
     describe '#to_a' do
       specify { expect(subject.to_a).to eql(initial_elements) }
+    end
+
+    describe '#min' do
+      specify { expect(subject.min).to be(initial_elements.min) }
+    end
+
+    describe '#include' do
+      specify { expect(subject.include?(initial_elements.first)).to be_truthy }
+    end
+
+    describe '#count' do
+      specify { expect(subject.count).to eql(initial_elements.count) }
+    end
+
+    describe '#sort' do
+      specify { expect(subject.sort).to eql(initial_elements.sort) }
+    end
+
+    describe '#delete' do
+      specify 'deletes an element' do
+        subject.delete(initial_elements.first)
+        expect(subject.to_a).to eql(initial_elements[1..-1])
+      end
+    end
+
+    context 'and the initial elements are not in sequence' do
+      let(:initial_elements) { [222, 33, 4] }
+
+      describe '#to_a' do
+        specify { expect(subject.to_a).to eql(initial_elements) }
+      end
+
+      describe '#sort' do
+        specify { expect(subject.sort).to eql(initial_elements.sort) }
+      end
     end
   end
 
