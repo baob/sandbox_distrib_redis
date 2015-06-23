834ba8f75d194f563c3722a5d32c2bf02ea37b9e
baob <coder@onesandthrees.com>
2015-06-23 23:18:02 +0100

    make integer store use same spec

--- spec/lib/redis_store/integer_store_spec.rb
+++ spec/lib/redis_store/integer_store_spec.rb
@@ -5,25 +5,5 @@ module RedisStore
 
     it_behaves_like 'an integer store'
 
-    context 'with two store initalised with the same id params' do
-      let(:a) { described_class.new(23) }
-      let(:b) { described_class.new(97) }
-
-      specify 'they use the same store' do
-        new_value = 967
-        expect { a.set(new_value).to change { b.to_i }.to(new_value) }
-      end
-    end
-
-    context 'with two store initalised with different id params' do
-      let(:a) { described_class.new(23, id: :a) }
-      let(:b) { described_class.new(97, id: :b) }
-
-      specify 'they use different stores' do
-        new_value = 967
-        expect { a.set(new_value).not_to change { b.to_i }.to(new_value) }
-      end
-    end
-
   end
 end
--- spec/support/shared_examples/integer_store.rb
+++ spec/support/shared_examples/integer_store.rb
@@ -17,4 +17,24 @@ shared_examples 'an integer store' do
     end
   end
 
+  context 'with two store initalised with the same id params' do
+    let(:a) { described_class.new(23) }
+    let(:b) { described_class.new(97) }
+
+    specify 'they use the same store' do
+      new_value = 967
+      expect { a.set(new_value).to change { b.to_i }.to(new_value) }
+    end
+  end
+
+  context 'with two store initalised with different id params' do
+    let(:a) { described_class.new(23, id: :a) }
+    let(:b) { described_class.new(97, id: :b) }
+
+    specify 'they use different stores' do
+      new_value = 967
+      expect { a.set(new_value).not_to change { b.to_i }.to(new_value) }
+    end
+  end
+
 end
