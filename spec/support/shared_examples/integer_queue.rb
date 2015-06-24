b55f23dd80c93795395f80b0baa93b5fc29b3369
baob <coder@onesandthrees.com>
2015-06-24 23:21:58 +0100

    re-organise integer queue spec

--- spec/support/shared_examples/integer_queue.rb
+++ spec/support/shared_examples/integer_queue.rb
@@ -6,49 +6,44 @@ shared_examples 'an integer queue' do
     end
   end
 
-  context 'when 3 elements are added in sequence' do
-    let(:initial_elements) { [2, 3, 4] }
+  context 'with elements' do
     before(:each) do
       initial_elements.each { |e| subject << e }
     end
 
-    describe '#empty?' do
-      specify { expect(subject.empty?).to be_falsey }
-    end
+    context 'when 3 elements are added in sequence' do
+      let(:initial_elements) { [2, 3, 4] }
 
-    describe '#to_a' do
-      specify { expect(subject.to_a).to eql(initial_elements) }
-    end
-
-    describe '#min' do
-      specify { expect(subject.min).to be(initial_elements.min) }
-    end
-
-    describe '#include' do
-      specify { expect(subject.include?(initial_elements.first)).to be_truthy }
-    end
-
-    describe '#count' do
-      specify { expect(subject.count).to eql(initial_elements.count) }
-    end
-
-    describe '#sort' do
-      specify { expect(subject.sort).to eql(initial_elements.sort) }
-    end
-
-    describe '#delete' do
-      specify 'deletes an element' do
-        subject.delete(initial_elements.first)
-        expect(subject.to_a).to eql(initial_elements[1..-1])
+      describe '#empty?' do
+        specify { expect(subject.empty?).to be_falsey }
       end
-    end
 
-    describe '#min_pop' do
-      specify { expect(subject.min_pop).to be(initial_elements.min) }
+      describe '#min' do
+        specify { expect(subject.min).to be(initial_elements.min) }
+      end
 
-      specify 'deletes an element' do
-        subject.min_pop
-        expect(subject.to_a).to eql(initial_elements[1..-1])
+      describe '#include' do
+        specify { expect(subject.include?(initial_elements.first)).to be_truthy }
+      end
+
+      describe '#sort' do
+        specify { expect(subject.sort).to eql(initial_elements.sort) }
+      end
+
+      describe '#delete' do
+        specify 'deletes an element' do
+          subject.delete(initial_elements.first)
+          expect(subject.to_a).to eql(initial_elements[1..-1])
+        end
+      end
+
+      describe '#min_pop' do
+        specify { expect(subject.min_pop).to be(initial_elements.min) }
+
+        specify 'deletes an element' do
+          subject.min_pop
+          expect(subject.to_a).to eql(initial_elements[1..-1])
+        end
       end
     end
 
@@ -63,6 +58,6 @@ shared_examples 'an integer queue' do
         specify { expect(subject.sort).to eql(initial_elements.sort) }
       end
     end
-  end
 
+  end
 end
